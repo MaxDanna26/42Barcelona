a29542919c683fb0b705a3f57143a0b9284f0e2b
MaxDanna26 <maximilianodanna@hotmail.com>
2024-05-01 09:47:32 +0200

    Finish ex07

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -1,26 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mdanna <mdanna@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/01 09:12:00 by mdanna            #+#    #+#             */
-/*   Updated: 2024/05/01 09:22:00 by mdanna           ###   ########.fr       */
+/*   Created: 2024/05/01 09:24:00 by mdanna            #+#    #+#             */
+/*   Updated: 2024/05/01 09:46:00 by mdanna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */ 
 
 #include <unistd.h>
 
-void    ft_strlen(char *str)
+void	ft_rev_int_tab(int *tab, int size)
 {
-    int     i;
-    
-    i = 0;
-    while (str[i] != '\0')
-    {
-        i++;
-    }
-    
-    return(i);
+	int	    aux;
+	int	    i;
+	int	    half;
+
+	i = 0;
+	aux = 0;
+	half = size / 2;
+
+	while (half-- > 0)
+	{
+		temp = tab [i];
+		tab [i++] = tab [size - 1];
+		tab [size-- -1] = temp;
+	}
 }
